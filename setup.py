378c89d65a61af5623c33c0036379a5e821fc84b
Meg Osato <megosato@Admins-Laptop.local>
2025-08-22 00:34:38 -0700

    add conda env

--- setup.py
+++ setup.py
@@ -1,8 +1,10 @@
-import setuptools
+from setuptools import setup, find_packages
 
-setuptools.setup(
-	name="slow_rotations",
-	version="0.0.1",
-	author="Meghan Osato, Travis Dabbous",
-	packages=["slow_rotations"]
+setup(
+    name="slow-rotations",
+    version="0.0.1",
+    packages=find_packages(),
+    install_requires=[
+        # list runtime dependencies here, e.g. numpy, rdkit
+    ],
 )

566a29f06949bf82af7361173f10e9ffba9d28db
Meg Osato <megosato@Admins-Laptop.local>
2025-10-26 21:35:10 -0700

    update env install

---
+++ slow_rotations/__init__.py
@@ -0,0 +1,18 @@
+# __init__.py
+
+"""slow rotations"""
+
+# Optional: package version
+__version__ = "0.1.0"
+
+# Import key functions/classes from submodules
+from .analysis import *
+from .compare import *
+from .mappings import *
+from .molconverter import *
+from .optimize_hyperparams import *
+from .pdb_writer import *
+from .rdkit_wrapper import *
+from .torsions import *
+from .transitions import *
+from .utils import *
